55a0c02ebfd9968de29f80a942f710f7223177b1
LaurenSUni <lauren.szmandiuk@student.uts.edu.au>
2024-04-05 16:12:57 +1100

    creating a theme

--- game_manager.gd
+++ game_manager.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 @onready var main_menu_instance = preload("res://levels/main_menu.tscn")
 var test_area_instance = preload("res://levels/top_down.tscn")
+var intro_cutscene_instance = preload("res://intro_cutscene.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	clear_game_values()
-	load_main_menu()
+	load_intro_cutscene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -24,5 +25,9 @@ func load_main_menu():
 	add_child(main_menu)
 	
 func load_test_area():
+	var test_area = test_area_instance.instantiate()
+	add_child(test_area)
+
+func load_intro_cutscene():
 	var test_area = test_area_instance.instantiate()
 	add_child(test_area)
