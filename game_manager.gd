6e40e1a5e0796ef1fba5ed06ea3c3e811e397150
LaurenSUni <lauren.szmandiuk@student.uts.edu.au>
2024-04-05 20:46:10 +1100

    main menu

--- game_manager.gd
+++ game_manager.gd
@@ -1,17 +1,28 @@
 extends Node2D
 
 @onready var main_menu_instance = preload("res://levels/main_menu.tscn")
+var options_menu_instance = preload("res://levels/options.tscn")
 var test_area_instance = preload("res://levels/top_down.tscn")
 var intro_cutscene_instance = preload("res://intro_cutscene.tscn")
+var pause_screen_instance = preload("res://levels/pause.tscn")
+
+enum GameStates {MAIN_MENU, MENU, GAMEPLAY}
+var game_state
+
+#change_scene_to_file()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	clear_game_values()
-	load_intro_cutscene()
+	load_main_menu()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if (Input.is_action_just_pressed("pause") and (game_state == GameStates.GAMEPLAY)):
+		#load_pause_screen()
+		pass
+	else:
+		pass
 
 func clear_game_values():
 	pass
@@ -23,11 +34,22 @@ func start_game():
 func load_main_menu():
 	var main_menu = main_menu_instance.instantiate()
 	add_child(main_menu)
+	game_state = GameStates.MAIN_MENU
 	
 func load_test_area():
 	var test_area = test_area_instance.instantiate()
 	add_child(test_area)
+	game_state = GameStates.GAMEPLAY
 
 func load_intro_cutscene():
 	var test_area = test_area_instance.instantiate()
 	add_child(test_area)
+	game_state = GameStates.GAMEPLAY
+	
+func load_options_menu():
+	var options_menu = options_menu_instance.instantiate()
+	add_child(options_menu)
+
+func load_pause_screen():
+	var pause_screen = pause_screen_instance.instantiate()
+	add_child(pause_screen)
--- main_menu.gd
+++ main_menu.gd
@@ -1,9 +1,20 @@
-extends Control
+extends CanvasLayer
 
-func _on_quit_pressed():
-	get_tree().quit()
+var game_manager
+
+func get_game_manager():
+	game_manager =  get_tree().root.get_child(0)
+
+func _ready():
+	get_game_manager()
 
 func _on_play_pressed():
 	print("a")
 	$"..".start_game()
 	queue_free()
+
+func _on_options_pressed():
+	game_manager.load_options_menu()
+
+func _on_quit_pressed():
+	get_tree().quit()
